b70957e721ebde807d6ddc44aa88393a882ff0d0
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2021-06-15 18:56:30 -0700

    added all files for the project

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 //Packages
 var http = require('http');
-// var http = require('https');
+var https = require('https');
+const fs = require('fs');
+const path = require('path');
 var MongoClient = require('mongodb').MongoClient;
 var gameDAO = require('./api/DAOs/game.DAO');
 require('dotenv').config({path: '.env'})
 
 //Variables
 const HTTP_PORT = process.env.PORT || process.env.HTTP_PORT;
-// const HTTPS_PORT = process.env.PORT || process.env.HTTPS_PORT;
+const HTTPS_PORT = process.env.PORT || process.env.HTTPS_PORT;
 
 //nodeEnvironment
 var nodeEnvironment = typeof(process.env.NODE_ENV)!= undefined ? process.env.NODE_ENV : 'development';
@@ -19,6 +21,19 @@ var app = require('./api/server');
 
 // HTTP Server
 const httpServer = http.createServer(app);
+var options = {
+    key: fs.readFileSync(path.join(__dirname, 'certificates','key.pem')),
+    cert: fs.readFileSync(path.join(__dirname,'certificates', 'cert.pem'))
+}
+const httpsServer = https.createServer(options,app);
+
+//Checking Certificates
+const {X509Certificate} = require('crypto');
+const x509 = new X509Certificate(fs.readFileSync(path.join(__dirname,'certificates', 'cert.pem')));
+const value = x509.verify(x509.publicKey);
+(value)
+?console.log(' ==> VALID X509 Certificate!')
+:console.log(' ==> IN-VALID X509 Certificate!')
 
 //MongoDB
 MongoClient.connect(
@@ -44,6 +59,9 @@ MongoClient.connect(
     //HTTP Server
     httpServer.listen(HTTP_PORT,function(){
         console.log(`[HTTP] app is listening on port => ${HTTP_PORT} in ${nodeEnvironment}`)
+    });
+    httpsServer.listen(HTTPS_PORT,function(){
+        console.log(`[HTTP] app is listening on port => ${HTTPS_PORT} in ${nodeEnvironment}`)
     });
     
 })
